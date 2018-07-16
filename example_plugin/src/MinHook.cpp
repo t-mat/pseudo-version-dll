8f229425672e5ae01968b8955bd43e19c202ffe1
Takayuki MATSUOKA <takayuki.matsuoka@gmail.com>
2018-07-17 03:22:56 +0900

    Added example plugin which hooks CreateFileW()

---
+++ example_plugin/src/MinHook.cpp
@@ -0,0 +1,61 @@
+﻿#include "MinHook.h"
+#include <stdio.h>
+
+namespace {
+	static const char* PluginManagerDll = "version.dll";
+
+	static HMODULE GetVersionDllHmodule() {
+		HMODULE h = nullptr;
+		if(GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, PluginManagerDll, &h)) {
+			return h;
+		}
+		return nullptr;
+	}
+}
+
+
+namespace MinHookApi {
+//	decltype(&Decl::MH_Initialize		) MH_Initialize			= nullptr;
+//	decltype(&Decl::MH_Uninitialize		) MH_Uninitialize		= nullptr;
+	decltype(&Decl::MH_CreateHook		) MH_CreateHook			= nullptr;
+	decltype(&Decl::MH_CreateHookApi	) MH_CreateHookApi		= nullptr;
+	decltype(&Decl::MH_CreateHookApiEx	) MH_CreateHookApiEx	= nullptr;
+	decltype(&Decl::MH_RemoveHook		) MH_RemoveHook			= nullptr;
+	decltype(&Decl::MH_EnableHook		) MH_EnableHook			= nullptr;
+	decltype(&Decl::MH_DisableHook		) MH_DisableHook		= nullptr;
+	decltype(&Decl::MH_QueueEnableHook	) MH_QueueEnableHook	= nullptr;
+	decltype(&Decl::MH_QueueDisableHook	) MH_QueueDisableHook	= nullptr;
+	decltype(&Decl::MH_ApplyQueued		) MH_ApplyQueued		= nullptr;
+	decltype(&Decl::MH_StatusToString	) MH_StatusToString		= nullptr;
+
+	MH_STATUS MH_Initialize() {
+		HMODULE	h = GetVersionDllHmodule();
+		if(h == nullptr) {
+			OutputDebugStringA("MH_Initialize : Can't load host DLL");
+			return MH_ERROR_NOT_INITIALIZED;
+		}
+	{
+        wchar_t buf[256];
+        swprintf_s(buf, L"GetVersionDllHmodule=%p\n",h);
+        OutputDebugStringW(buf);
+	}
+
+//		* reinterpret_cast<void**>(&MH_Initialize		) = GetProcAddress(h, "_MH_Initialize");
+//		* reinterpret_cast<void**>(&MH_Uninitialize		) = GetProcAddress(h, "_MH_Uninitialize");
+		* reinterpret_cast<void**>(&MH_CreateHook		) = GetProcAddress(h, "_MH_CreateHook");
+		* reinterpret_cast<void**>(&MH_CreateHookApi	) = GetProcAddress(h, "_MH_CreateHookApi");
+		* reinterpret_cast<void**>(&MH_CreateHookApiEx	) = GetProcAddress(h, "_MH_CreateHookApiEx");
+		* reinterpret_cast<void**>(&MH_RemoveHook		) = GetProcAddress(h, "_MH_RemoveHook");
+		* reinterpret_cast<void**>(&MH_EnableHook		) = GetProcAddress(h, "_MH_EnableHook");
+		* reinterpret_cast<void**>(&MH_DisableHook		) = GetProcAddress(h, "_MH_DisableHook");
+		* reinterpret_cast<void**>(&MH_QueueEnableHook	) = GetProcAddress(h, "_MH_QueueEnableHook");
+		* reinterpret_cast<void**>(&MH_QueueDisableHook	) = GetProcAddress(h, "_MH_QueueDisableHook");
+		* reinterpret_cast<void**>(&MH_ApplyQueued		) = GetProcAddress(h, "_MH_ApplyQueued");
+		* reinterpret_cast<void**>(&MH_StatusToString	) = GetProcAddress(h, "_MH_StatusToString");
+		return MH_OK;
+	}
+
+	MH_STATUS MH_Uninitialize() {
+		return MH_OK;
+	}
+}
---
+++ example_plugin/src/dllmain.cpp
@@ -0,0 +1,83 @@
+﻿#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#ifndef _WIN64
+#  error This code must be compiled with x64 configuration
+#endif
+#pragma comment(linker, "/DLL")
+#include "minhook.h"
+#include <stdio.h>
+
+
+namespace Original {
+    decltype(&CreateFileW) CreateFileW {};
+}
+
+
+namespace Detour {
+    HANDLE WINAPI CreateFileW(
+        LPCWSTR               lpFileName,
+        DWORD                 dwDesiredAccess,
+        DWORD                 dwShareMode,
+        LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+        DWORD                 dwCreationDisposition,
+        DWORD                 dwFlagsAndAttributes,
+        HANDLE                hTemplateFile
+    ) {
+        wchar_t buf[256];
+        swprintf_s(buf, L"Example Plugin Hook: CreateFileW(%s, %08x)", lpFileName, dwDesiredAccess);
+        OutputDebugStringW(buf);
+        return Original::CreateFileW(
+            lpFileName,
+            dwDesiredAccess,
+            dwShareMode,
+            lpSecurityAttributes,
+            dwCreationDisposition,
+            dwFlagsAndAttributes,
+            hTemplateFile
+        );
+    }
+}
+
+
+void attach() {
+    using namespace MinHookApi;
+OutputDebugStringA("ATTACH: 1");
+    MH_Initialize();
+OutputDebugStringA("ATTACH: 2");
+	{
+        wchar_t buf[256];
+        swprintf_s(buf, L"MH_CreateHook=%p\n", MH_CreateHook);
+        OutputDebugStringW(buf);
+	}
+    MH_CreateHook(&CreateFileW, &Detour::CreateFileW, reinterpret_cast<void**>(&Original::CreateFileW));
+OutputDebugStringA("ATTACH: 3");
+    MH_EnableHook(&CreateFileW);
+OutputDebugStringA("ATTACH: 4");
+}
+
+
+void detach() {
+    using namespace MinHookApi;
+    MH_DisableHook(&MessageBoxW);
+}
+
+
+BOOL APIENTRY DllMain(HMODULE, DWORD ul_reason_for_call, LPVOID) {
+    switch(ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        attach();
+        break;
+
+    case DLL_PROCESS_DETACH:
+        detach();
+        break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    default:
+        break;
+    }
+
+    return TRUE;
+}
