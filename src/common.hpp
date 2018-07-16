f2df9dce8d5f4f0b0b489a1f5185cc84b5c4d270
Takayuki MATSUOKA <takayuki.matsuoka@gmail.com>
2018-07-17 03:17:01 +0900

    Added config.h to configure build settings

--- src/common.hpp
+++ src/common.hpp
@@ -3,16 +3,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #pragma comment(linker, "/DLL")
-//
-// If you want to use "plugin" system, define the following macro USE_VERSION_DLL_HOOK_PLUGIN.
-// When you define this macro, pseudo version.dll will load DLLs from the following path (recursively, dictionary order).
-//    <PATH-OF-YOUR-version.dll>\version.dll.plugins\**.dll
-// All native DLLs are loaded by LoadLibrary(), and unloaded by FreeLibrary().
-//
-// Unless, comment out the following line.  All plugin system completely removed.
-//
-#define USE_VERSION_DLL_HOOK_PLUGIN 1
-#define USE_DEBUG_TRACE
 
+#include "config.h"
 #include "outputdebugstring.hpp"
-#define APPNAME L"PSEUDO-version.dll"
+#define APPNAME "PSEUDO-version.dll"
