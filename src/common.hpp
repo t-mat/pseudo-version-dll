6fa5551d0767eac9ee9df6a45bb0aaf0841bd29e
Takayuki Matsuoka <t-mat@users.noreply.github.com>
2022-06-04 22:43:27 +0900

    Update common.hpp

--- src/common.hpp
+++ src/common.hpp
@@ -1,10 +1,20 @@
 #pragma once
-#include <algorithm>
 #include <SDKDDKVer.h>
 #define WIN32_LEAN_AND_MEAN
+#define NOGDI
 #include <windows.h>
+#include <stdio.h>
+#include <algorithm>
 #pragma comment(linker, "/DLL")
 
 #include "config.h"
 #include "outputdebugstring.hpp"
 #define APPNAME "PSEUDO-version.dll"
+
+enum class DllType {
+    Unknown,
+    Version,
+    Lz32,
+    Hid,
+	Msimg32,
+};
