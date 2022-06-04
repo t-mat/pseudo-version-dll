e21b03ace7465b4c4d406c1903cb0f488a2284d7
Takayuki Matsuoka <t-mat@users.noreply.github.com>
2022-06-04 22:43:36 +0900

    Update config.h

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 //
 // If you want to use "plugin" system, define the following macro USE_VERSION_DLL_HOOK_PLUGIN.
 // When you define this macro, pseudo version.dll will load DLLs from the following path (recursively, dictionary order).
-//    <PATH-OF-YOUR-version.dll>\version.dll.plugins\**.dll
+//    <PATH-OF-YOUR-version.dll>\<YOUR-TARGET-executable>.exe.plugins\**.dll
 // All native DLLs are loaded by LoadLibrary(), and unloaded by FreeLibrary().
 //
 // Unless, comment out the following line.  All plugin system completely removed.
@@ -14,6 +14,30 @@
 // If you want to use MinHook, define the following macro USE_MINHOOK.
 //
 #define USE_MINHOOK 1
+
+
+//
+// If you define and set '1' to EXPORT_VERSION_DLL_FUNCTIONS, DLL exports version.dll functions
+//
+#define EXPORT_VERSION_DLL_FUNCTIONS 1
+
+
+//
+// If you define and set '1' to EXPORT_LZ32_DLL_FUNCTIONS, DLL exports lz32.dll functions
+//
+#define EXPORT_LZ32_DLL_FUNCTIONS 1
+
+
+//
+// If you define and set '1' to EXPORT_HID_DLL_FUNCTIONS, DLL exports hid.dll functions
+//
+#define EXPORT_HID_DLL_FUNCTIONS 1
+
+
+//
+// If you define and set '1' to EXPORT_MSIMG32_DLL_FUNCTIONS, DLL exports msimg32.dll functions
+//
+#define EXPORT_MSIMG32_DLL_FUNCTIONS 1
 
 
 #if defined(_DEBUG)
